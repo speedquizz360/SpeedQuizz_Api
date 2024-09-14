69bb909a065a537e444ed10594ad50e5184632f7
speedquizz360 <speedquizz360@gmail.com>
2024-09-14 22:18:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -67,10 +67,6 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to speedquizz application." });
 });
 
-app.get("/lp", (req, res) => {
-  res.json({ message: "Welcome to speedquizz application LP." });
-});
-
 // routes
 require("./routes/auth.routes")(app);
 require("./routes/user.routes")(app);
